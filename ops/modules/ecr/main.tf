39e6b7023551e17a094d01ad49933beaea1eb915
just-a-node <eye1717@gmail.com>
2025-01-16 17:51:57 -0700

    feat: add ecr module

---
+++ ops/modules/ecr/main.tf
@@ -0,0 +1,27 @@
+resource "aws_ecr_repository" "mark_poller" {
+  name                 = "mark-poller"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "mark_poller" {
+  repository = aws_ecr_repository.mark_poller.name
+
+  policy = jsonencode({
+    rules = [{
+      rulePriority = 1
+      description  = "Keep last 30 images"
+      selection = {
+        tagStatus   = "any"
+        countType   = "imageCountMoreThan"
+        countNumber = 30
+      }
+      action = {
+        type = "expire"
+      }
+    }]
+  })
+}
