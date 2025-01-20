75d0ec0943c0c16319b62041a262d952c2f5e773
Prathmesh <201952225@iiitvadodara.ac.in>
2025-01-20 14:39:38 +0530

    feat: test scripts

---
+++ packages/poller/test/helpers/mock.spec.ts
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+import sinon, { SinonStub } from 'sinon';
+
+describe('#TEST', () => {
+  describe('#getUnauthenticatedTokenPrice', () => {
+    it('should return 2 when adding 1 + 1', () => {
+      expect(1 + 1).to.eq(2);
+    });
+  });
+});
