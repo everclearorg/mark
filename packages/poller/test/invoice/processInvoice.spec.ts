a764c1b68cb80830cacb87510d6c09272a05c206
Layne Haber <layne.haber@gmail.com>
2025-01-22 10:35:24 -0700

    fix: invoice types aligned with api

--- packages/poller/test/invoice/processInvoice.spec.ts
+++ packages/poller/test/invoice/processInvoice.spec.ts
@@ -1,105 +1,86 @@
 import { expect } from 'chai';
-import { isValidInvoice, Invoice } from '../../src/invoice/processInvoices';
+import { isValidInvoice } from '../../src/invoice/processInvoices';
+import { Invoice } from '@mark/everclear';
+import { MarkConfiguration } from '@mark/core';
+
+const validInvoice = {
+  intent_id: '0x60d2ec64161aed1c3846304775134d9da6d716b1f718176e6f24cb34b26950d0',
+  owner: '0xe358babAbc57442a25Ab72196a9F80ff1c730300',
+  entry_epoch: 186595,
+  amount: '4506224658731513369685',
+  discountBps: 1.2,
+  origin: '1',
+  destinations: ['8453'],
+  hub_status: 'INVOICED',
+  ticker_hash: '0xd6aca1be9729c13d677335161321649cccae6a591554772516700f986f942eaa',
+  hub_invoice_enqueued_timestamp: 1737491219,
+};
+
+const validConfig = {
+  signer: '0xMark',
+  supportedSettlementDomains: [8453],
+} as MarkConfiguration;
 
 describe('isValidInvoice', () => {
   it('should return true for a valid invoice', () => {
-    const validInvoice: Invoice = {
-      id: 'invoice123',
-      amount: 100,
-      chainId: '1',
-      owner: 'someOwner',
-      destinations: ['2', '3'],
-      ticker_hash: '0xhash',
-    };
-
-    const result = isValidInvoice(validInvoice);
+    const result = isValidInvoice(validInvoice, validConfig);
     expect(result).to.be.true;
   });
 
   it('should return false if invoice is null or undefined', () => {
-    const resultNull = isValidInvoice(null as any);
-    const resultUndefined = isValidInvoice(undefined as any);
+    const resultNull = isValidInvoice(null as any, validConfig);
+    const resultUndefined = isValidInvoice(undefined as any, validConfig);
 
     expect(resultNull).to.be.false;
     expect(resultUndefined).to.be.false;
   });
 
   it('should return false if id is not a string', () => {
+    const { intent_id, ...res } = validInvoice;
     const invalidInvoice: Partial<Invoice> = {
-      id: 123 as any, // Invalid id type
-      amount: 100,
-      chainId: '1',
-      owner: 'someOwner',
-      destinations: ['2', '3'],
-      ticker_hash: '0xhash',
+      intent_id: 123 as any, // Invalid id type
+      ...res,
     };
 
-    const result = isValidInvoice(invalidInvoice as Invoice);
+    const result = isValidInvoice(invalidInvoice as Invoice, validConfig);
     expect(result).to.be.false;
   });
 
   it('should return false if amount is not a number', () => {
+    const { amount, ...res } = validInvoice;
     const invalidInvoice: Partial<Invoice> = {
-      id: 'invoice123',
-      amount: '100' as any, // Invalid amount type
-      chainId: '1',
-      owner: 'someOwner',
-      destinations: ['2', '3'],
-      ticker_hash: '0xhash',
+      amount: 'abc',
+      ...res,
     };
 
-    const result = isValidInvoice(invalidInvoice as Invoice);
-    expect(result).to.be.false;
-  });
-
-  it('should return false if amount is not a valid number', () => {
-    const invalidInvoice: Invoice = {
-      id: 'invoice123',
-      amount: 'abc' as any, // Invalid amount
-      chainId: '1',
-      owner: 'someOwner',
-      destinations: ['2', '3'],
-      ticker_hash: '0xhash',
-    };
-
-    const result = isValidInvoice(invalidInvoice);
+    const result = isValidInvoice(invalidInvoice as Invoice, validConfig);
     expect(result).to.be.false;
   });
 
   it('should return false if amount is less than or equal to 0', () => {
+    const { amount, ...res } = validInvoice;
     const invalidInvoiceZero: Invoice = {
-      id: 'invoice123',
-      amount: 0, // Zero amount
-      chainId: '1',
-      owner: 'someOwner',
-      destinations: ['2', '3'],
-      ticker_hash: '0xhash',
+      amount: '0',
+      ...res,
     };
 
     const invalidInvoiceNegative: Invoice = {
-      id: 'invoice123',
-      amount: -50, // Negative amount
-      chainId: '1',
-      owner: 'someOwner',
-      destinations: ['2', '3'],
-      ticker_hash: '0xhash',
+      amount: '-50',
+      ...res,
     };
 
-    expect(isValidInvoice(invalidInvoiceZero)).to.be.false;
-    expect(isValidInvoice(invalidInvoiceNegative)).to.be.false;
+    expect(isValidInvoice(invalidInvoiceZero, validConfig)).to.be.false;
+    expect(isValidInvoice(invalidInvoiceNegative, validConfig)).to.be.false;
   });
 
   it('should return false if owner matches "Mark wallet address"', () => {
+    const { owner, ...res } = validInvoice;
     const invalidInvoice: Invoice = {
-      id: 'invoice123',
-      amount: 100,
-      chainId: '1',
       owner: 'Mark wallet address', // Invalid owner
-      destinations: ['2', '3'],
-      ticker_hash: '0xhash',
+      ...res,
     };
 
-    const result = isValidInvoice(invalidInvoice);
+    const result = isValidInvoice(invalidInvoice, validConfig);
     expect(result).to.be.false;
   });
 });
