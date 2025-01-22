24358962fd08fdf802f4674e7f4ef48a606924e7
Layne Haber <layne.haber@gmail.com>
2025-01-22 13:39:57 -0700

    feat: wip logic

---
+++ packages/poller/src/helpers/asset.ts
@@ -0,0 +1,7 @@
+/**
+ * Returns true if the XERC20 strategy is supported on any of the domains for the
+ * given ticker hash
+ */
+export const isXerc20Supported = async (ticker: string, domains: string[]): Promise<boolean> => {
+  throw new Error('isXerc20Supported not implemented');
+};
--- packages/poller/src/helpers/balance.ts
+++ packages/poller/src/helpers/balance.ts
@@ -12,37 +12,21 @@ export const walletBalance = async (tokenAddress: string, chainId: string, confi
 };
 
 /**
- * Returns all of the balances for supported assets across all chains
+ * Returns all of the balances for supported assets across all chains.
+ * @returns Mapping of balances keyed on tickerhash - chain - amount
  */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const getMarkBalances = async (config: MarkConfiguration): Promise<Map<string, Map<string, string>>> => {
+export const getMarkBalances = async (config: MarkConfiguration): Promise<Map<string, Map<string, bigint>>> => {
   // const { chains, supportedAssets } = config;
   throw new Error(`not implemented - getMarkBalances`);
 };
 
-export const markHighestLiquidityBalance = async (
-  ticker_hash: string,
-  origins: string[],
-  config: MarkConfiguration,
-  getTokenAddressFn: (ticker_hash: string, origin: string) => Promise<string> | string,
-) => {
-  try {
-    let highestLiquidityDomain = 0;
-    let amount = 0;
-
-    for (const origin of origins) {
-      const tokenAddress = await getTokenAddressFn(ticker_hash, origin);
-      const tokenContract = await getERC20Contract(config, origin, tokenAddress as `0x${string}`);
-      const balance = (await tokenContract.read.balanceOf()) as number;
-      if (balance > amount) {
-        amount = balance;
-        highestLiquidityDomain = +origin;
-      }
-    }
-
-    return highestLiquidityDomain;
-  } catch (err) {
-    console.log('Not able to fetch the wallet balance!', err);
-    return 0;
-  }
+/**
+ * Returns all of the custodied amounts for supported assets across all chains
+ * @returns Mapping of balances keyed on tickerhash - chain - amount
+ */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const getCustodiedBalances = async (config: MarkConfiguration): Promise<Map<string, Map<string, bigint>>> => {
+  // const { chains, supportedAssets } = config;
+  throw new Error(`not implemented - getCustodiedBalances`);
 };
