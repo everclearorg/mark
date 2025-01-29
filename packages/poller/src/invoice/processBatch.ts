e02507151c3f85a84f6c341cbf9604e3fd1caec2
Layne Haber <layne.haber@gmail.com>
2025-01-29 14:09:44 -0700

    feat: assert balance, gas; check invoice age by domain

--- packages/poller/src/invoice/processBatch.ts
+++ packages/poller/src/invoice/processBatch.ts
@@ -2,20 +2,39 @@ import { MarkConfiguration, NewIntentParams, getTokenAddressFromConfig } from '@
 import { Invoice } from '@mark/everclear';
 import { ProcessInvoicesDependencies } from './pollAndProcess';
 import { isValidInvoice } from './validation';
-import { combineIntents, getCustodiedBalances, getMarkBalances, isXerc20Supported, sendIntents } from '../helpers';
+import {
+  combineIntents,
+  getCustodiedBalances,
+  getMarkBalances,
+  getMarkGasBalances,
+  isXerc20Supported,
+  logBalanceThresholds,
+  logGasThresholds,
+  sendIntents,
+} from '../helpers';
 import { jsonifyMap } from '@mark/logger';
 
 export async function processBatch(
-  invoices: Invoice[],
+  _invoices: Invoice[],
   deps: ProcessInvoicesDependencies,
   config: MarkConfiguration,
 ): Promise<void> {
   const { logger } = deps;
 
+  // Sort invoices so oldest enqueued is first
+  const invoices = _invoices.sort((a, b) => a.hub_invoice_enqueued_timestamp - b.hub_invoice_enqueued_timestamp);
+
   // Query all of marks balances across chains
   logger.info('Getting mark balances', { chains: Object.keys(config.chains) });
   const balances = await getMarkBalances(config);
+  logBalanceThresholds(balances, config, logger);
   logger.debug('Retrieved balances', { balances: jsonifyMap(balances) });
+
+  // Query all of marks gas balances across chains
+  logger.info('Getting mark gas balances', { chains: Object.keys(config.chains) });
+  const gas = await getMarkGasBalances(config);
+  logGasThresholds(gas, config, logger);
+  logger.debug('Retrieved gas balances', { balances: jsonifyMap(gas) });
 
   // Query all of the custodied amounts across chains and ticker hashes
   logger.info('Getting custodied balances', { chains: Object.keys(config.chains) });
@@ -26,10 +45,15 @@ export async function processBatch(
   // invoices, grouped by origin domain
   const unbatchedIntents = new Map<string, NewIntentParams[]>();
 
+  // Get current time to measure invoice age
+  const time = Math.floor(Date.now() / 1000);
+
   for (const invoice of invoices) {
-    if (!isValidInvoice(invoice, config)) {
-      logger.info('Invalid invoice', {
+    const msg = isValidInvoice(invoice, config);
+    if (msg) {
+      logger.warn('Invalid invoice', {
         invoice,
+        msg,
       });
       continue;
     }
@@ -62,6 +86,18 @@ export async function processBatch(
     // TODO: should look at all enqueued intents for each destination that could be processed before
     // marks created intent - this can come from /economy endpoint
     for (const destination of destinations) {
+      // Verify invoice is old enough to consider
+      const age = time - invoice.hub_invoice_enqueued_timestamp;
+      if (age < (config.chains[destination]?.invoiceAge ?? time)) {
+        logger.info('Invoice not old enough to settle on this domain', {
+          domain: destination,
+          threshold: config.chains[destination]?.invoiceAge,
+          age,
+          invoice,
+        });
+        continue;
+      }
+
       const destinationCustodied = BigInt(custodied.get(invoice.ticker_hash.toLowerCase())?.get(destination) ?? '0');
       // If there is sufficient custodied asset, ignore invoice. should be settlable w.o intervention.
       if (purchaseable <= destinationCustodied) {
