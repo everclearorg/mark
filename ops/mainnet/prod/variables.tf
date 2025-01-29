1871c0f1edcbedb02280f529defcc2ba7a5712e1
Layne Haber <layne.haber@gmail.com>
2025-01-29 12:29:29 -0700

    fix: settle all chain

--- ops/mainnet/prod/variables.tf
+++ ops/mainnet/prod/variables.tf
@@ -73,7 +73,7 @@ variable "relayer_api_key" {
 variable "supported_settlement_domains" {
   description = "Comma-separated list of supported settlement domains"
   type        = string
-  default     = "1,56"
+  default     = "1,10,56,8453,42161,48900,59144,137,43114,81457"
 }
 
 variable "supported_assets" {
@@ -91,7 +91,7 @@ variable "log_level" {
 variable "chain_ids" {
   description = "Comma-separated list of chain IDs"
   type        = string
-  default     = "1,56"
+  default     = "1,10,56,8453,42161,48900,59144,137,43114,81457"
 }
 
 variable "dd_api_key" {
