f11cb10e19b4744a2fe09e1409913d079731397c
just-a-node <eye1717@gmail.com>
2025-02-06 14:37:12 -0700

    feat: mark creates unique ticker+dest intents, no need to batch now

--- packages/poller/src/helpers/intent.ts
+++ packages/poller/src/helpers/intent.ts
@@ -59,7 +59,7 @@ export const combineIntents = async (
  * Uses the api to get the tx data and chainservice to send intents and approve assets if required. Takes in the origin-asset batched intents.
  */
 export const sendIntents = async (
-  batch: Map<string, Map<string, NewIntentParams>>,
+  intents: NewIntentParams[],
   deps: ProcessInvoicesDependencies,
   config: MarkConfiguration,
 ): Promise<{ transactionHash: string; chainId: string }[]> => {
@@ -67,83 +67,80 @@ export const sendIntents = async (
   const results: { transactionHash: string; chainId: string }[] = [];
 
   try {
-    for (const [domain, originMap] of batch.entries()) {
-      logger.info('Attempting to send batched intents', { domain, intents: originMap.size });
-      for (const intent of originMap.values()) {
-        logger.info('Processing intent for new transaction', { intent });
+    for (const intent of intents) {
+      logger.info('Processing intent for new transaction', { intent });
 
-        // Fetch transaction data for creating a new intent
-        const txData = await everclear.createNewIntent(intent);
+      // Fetch transaction data for creating a new intent
+      const txData = await everclear.createNewIntent(intent);
 
-        logger.debug('Received transaction data for new intent', { txData });
+      logger.debug('Received transaction data for new intent', { txData });
 
-        // Ensure allowance for the transaction
-        const tokenContract = await getERC20Contract(config, intent.origin, intent.inputAsset as `0x${string}`);
-        const allowance = await tokenContract.read.allowance([config.ownAddress, txData.to]);
+      // Ensure allowance for the transaction
+      const tokenContract = await getERC20Contract(config, intent.origin, intent.inputAsset as `0x${string}`);
+      const allowance = await tokenContract.read.allowance([config.ownAddress, txData.to]);
 
-        if (BigInt(allowance as string) < BigInt(intent.amount)) {
-          logger.info('Allowance insufficient, preparing approval transaction', {
-            requiredAmount: intent.amount,
-            currentAllowance: allowance,
-          });
-
-          const approveCalldata = encodeFunctionData({
-            abi: erc20Abi,
-            functionName: 'approve',
-            args: [txData.to as `0x${string}`, BigInt(intent.amount)],
-          });
-          const transaction = {
-            to: tokenContract.address,
-            data: approveCalldata,
-            from: config.ownAddress,
-          };
-
-          logger.debug('Sending approval transaction', { transaction, chainId: txData.chainId });
-          const approvalTxHash = await chainService.submitAndMonitor(txData.chainId.toString(), transaction);
-
-          logger.info('Approval transaction sent successfully', {
-            chain: txData.chainId,
-            approvalTxHash,
-            allowance,
-            asset: tokenContract.address,
-            amount: intent.amount,
-          });
-        } else {
-          logger.info('Sufficient allowance already available', {
-            allowance,
-            chain: txData.chainId,
-            asset: tokenContract.address,
-            amount: intent.amount,
-          });
-        }
-
-        // Submit the create intent transaction
-        logger.info('Submitting create intent transaction', {
-          intent,
-          transaction: {
-            to: txData.to,
-            value: txData.value,
-            data: txData.data,
-            from: txData.from,
-            chain: txData.chainId,
-          },
+      if (BigInt(allowance as string) < BigInt(intent.amount)) {
+        logger.info('Allowance insufficient, preparing approval transaction', {
+          requiredAmount: intent.amount,
+          currentAllowance: allowance,
         });
 
-        const intentTxHash = await chainService.submitAndMonitor(txData.chainId.toString(), {
-          to: txData.to as string,
-          value: txData.value ?? '0',
-          data: txData.data,
-          from: txData.from ?? config.ownAddress,
+        const approveCalldata = encodeFunctionData({
+          abi: erc20Abi,
+          functionName: 'approve',
+          args: [txData.to as `0x${string}`, BigInt(intent.amount)],
         });
+        const transaction = {
+          to: tokenContract.address,
+          data: approveCalldata,
+          from: config.ownAddress,
+        };
 
-        logger.info('Create intent transaction sent successfully', {
-          intentTxHash,
-          chainId: intent.origin,
+        logger.debug('Sending approval transaction', { transaction, chainId: txData.chainId });
+        const approvalTxHash = await chainService.submitAndMonitor(txData.chainId.toString(), transaction);
+
+        logger.info('Approval transaction sent successfully', {
+          chain: txData.chainId,
+          approvalTxHash,
+          allowance,
+          asset: tokenContract.address,
+          amount: intent.amount,
         });
-
-        // Add result to the output array
-        results.push({ transactionHash: intentTxHash, chainId: intent.origin });
+      } else {
+        logger.info('Sufficient allowance already available', {
+          allowance,
+          chain: txData.chainId,
+          asset: tokenContract.address,
+          amount: intent.amount,
+        });
       }
+
+      // Submit the create intent transaction
+      logger.info('Submitting create intent transaction', {
+        intent,
+        transaction: {
+          to: txData.to,
+          value: txData.value,
+          data: txData.data,
+          from: txData.from,
+          chain: txData.chainId,
+        },
+      });
+
+      const intentTxHash = await chainService.submitAndMonitor(txData.chainId.toString(), {
+        to: txData.to as string,
+        value: txData.value ?? '0',
+        data: txData.data,
+        from: txData.from ?? config.ownAddress,
+      });
+
+      logger.info('Create intent transaction sent successfully', {
+        intentTxHash,
+        chainId: intent.origin,
+      });
+
+      // Add result to the output array
+      results.push({ transactionHash: intentTxHash, chainId: intent.origin });
     }
     return results;
   } catch (err) {
