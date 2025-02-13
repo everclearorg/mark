6f57cbafebb4f1cb402d291096e9436eb81037f9
Layne Haber <layne.haber@gmail.com>
2025-02-12 17:05:16 -0700

    refactor: use chaindata, only threshold from env

--- ops/mainnet/prod/config.tf
+++ ops/mainnet/prod/config.tf
@@ -20,23 +20,31 @@ locals {
     DD_LAMBDA_HANDLER             = "packages/poller/dist/index.handler"
     
     CHAIN_1_PROVIDERS             = "https://eth-mainnet.blastapi.io/${var.blast_key}, https://eth-mainnet.g.alchemy.com/v2/${var.alchemy_key}"
-    CHAIN_1_ASSETS                = "WETH,0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2,18,0x0f8a193ff464434486c0daf7db2a895884365d2bc84ba47a68fcf89c1b14b5b8,FALSE,800000000000000000;USDC,0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48,6,0xd6aca1be9729c13d677335161321649cccae6a591554772516700f986f942eaa,FALSE,4000000000;USDT,0xdAC17F958D2ee523a2206206994597C13D831ec7,6,0x8b1a1d9c2b109e527c9134b25b1a1833b16b6594f92daa9f6d9b7a6024bce9d0,FALSE,2000000000"
+    WETH_1_THRESHOLD              = "800000000000000000"
+    USDC_1_THRESHOLD              = "4000000000"
+    USDT_1_THRESHOLD              = "2000000000"
     
     CHAIN_10_PROVIDERS            = "https://optimism-mainnet.blastapi.io/${var.blast_key}, https://opt-mainnet.g.alchemy.com/v2/${var.alchemy_key}"
-    CHAIN_10_ASSETS               = "WETH,0x4200000000000000000000000000000000000006,18,0x0f8a193ff464434486c0daf7db2a895884365d2bc84ba47a68fcf89c1b14b5b8,FALSE,1600000000000000000;USDC,0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85,6,0xd6aca1be9729c13d677335161321649cccae6a591554772516700f986f942eaa,FALSE,4000000000;USDT,0x94b008aA00579c1307B0EF2c499aD98a8ce58e58,6,0x8b1a1d9c2b109e527c9134b25b1a1833b16b6594f92daa9f6d9b7a6024bce9d0,FALSE,400000000"
+    WETH_10_THRESHOLD             = "1600000000000000000"
+    USDC_10_THRESHOLD             = "4000000000"
+    USDT_10_THRESHOLD             = "400000000"
     
     CHAIN_56_PROVIDERS            = "https://bsc-mainnet.blastapi.io/${var.blast_key}, https://bnb-mainnet.g.alchemy.com/v2/${var.alchemy_key}"
-    CHAIN_56_ASSETS               = "WETH,0x2170Ed0880ac9A755fd29B2688956BD959F933F8,18,0x0f8a193ff464434486c0daf7db2a895884365d2bc84ba47a68fcf89c1b14b5b8,FALSE,0;USDC,0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d,18,0xd6aca1be9729c13d677335161321649cccae6a591554772516700f986f942eaa,FALSE,2000000000000000000000;USDT,0x55d398326f99059fF775485246999027B3197955,18,0x8b1a1d9c2b109e527c9134b25b1a1833b16b6594f92daa9f6d9b7a6024bce9d0,FALSE,4000000000000000000000"
+    USDC_56_THRESHOLD             = "2000000000000000000000"
+    USDT_56_THRESHOLD             = "4000000000000000000000"
     
     CHAIN_137_PROVIDERS           = "https://polygon-mainnet.blastapi.io/${var.blast_key}, https://polygon-mainnet.g.alchemy.com/v2/${var.alchemy_key}"
 
     CHAIN_8453_PROVIDERS          = "https://base-mainnet.blastapi.io/${var.blast_key}, https://base-mainnet.g.alchemy.com/v2/${var.alchemy_key}"
-    CHAIN_8453_ASSETS             = "WETH,0x4200000000000000000000000000000000000006,18,0x0f8a193ff464434486c0daf7db2a895884365d2bc84ba47a68fcf89c1b14b5b8,FALSE,1600000000000000000;USDC,0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913,6,0xd6aca1be9729c13d677335161321649cccae6a591554772516700f986f942eaa,FALSE,4000000000;USDT,0xfde4C96c8593536E31F229EA8f37b2ADa2699bb2,6,0x8b1a1d9c2b109e527c9134b25b1a1833b16b6594f92daa9f6d9b7a6024bce9d0,FALSE,0"
+    WETH_8453_THRESHOLD           = "1600000000000000000"
+    USDC_8453_THRESHOLD           = "4000000000"
     
     CHAIN_33139_PROVIDERS         = "https://apechain-mainnet.blastapi.io/${var.blast_key}, https://apechain-mainnet.g.alchemy.com/v2/${var.alchemy_key}"
 
     CHAIN_42161_PROVIDERS         = "https://arbitrum-one.blastapi.io/${var.blast_key}, https://arb-mainnet.g.alchemy.com/v2/${var.alchemy_key}"
-    CHAIN_42161_ASSETS            = "WETH,0x82aF49447D8a07e3bd95BD0d56f35241523fBab1,18,0x0f8a193ff464434486c0daf7db2a895884365d2bc84ba47a68fcf89c1b14b5b8,FALSE,1600000000000000000;USDC,0xaf88d065e77c8cC2239327C5EDb3A432268e5831,6,0xd6aca1be9729c13d677335161321649cccae6a591554772516700f986f942eaa,FALSE,4000000000;USDT,0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9,6,0x8b1a1d9c2b109e527c9134b25b1a1833b16b6594f92daa9f6d9b7a6024bce9d0,FALSE,1000000000"
+    WETH_42161_THRESHOLD          = "1600000000000000000"
+    USDC_42161_THRESHOLD          = "4000000000"
+    USDT_42161_THRESHOLD          = "1000000000"
     
     CHAIN_43114_PROVIDERS         = "https://ava-mainnet.blastapi.io/${var.blast_key}/ext/bc/C/rpc, https://avalanche-mainnet.g.alchemy.com/v2/${var.alchemy_key}"
 
