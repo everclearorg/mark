3c39c7013983b8443d36201565c1081f40444a3d
Layne Haber <layne.haber@gmail.com>
2025-02-24 12:55:56 -0700

    feat: add zksync

--- ops/mainnet/prod/config.tf
+++ ops/mainnet/prod/config.tf
@@ -117,7 +117,9 @@ locals {
 
     CHAIN_534352_PROVIDERS        = "https://scroll-mainnet.g.alchemy.com/v2/${var.alchemy_key}"
 
-    CHAIN_34443_PROVIDERS        = "https://mode-mainnet.blastapi.io/${var.blast_key}, https://mainnet.mode.network"
+    CHAIN_34443_PROVIDERS         = "https://mode-mainnet.blastapi.io/${var.blast_key}, https://mainnet.mode.network"
+
+    CHAIN_324_PROVIDERS           = "https://zksync-mainnet.g.alchemy.com/v2/${var.alchemy_key}, https://mainnet.era.zksync.io"
   }
 
   web3signer_env_vars = [
