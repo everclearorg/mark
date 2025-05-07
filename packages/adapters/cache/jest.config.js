345c6a45fe833c3b7877d25e322fe7c028e0e684
Layne Haber <layne.haber@gmail.com>
2025-05-07 14:33:23 -0600

    feat: add coverage

--- packages/adapters/cache/jest.config.js
+++ packages/adapters/cache/jest.config.js
@@ -2,11 +2,25 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     testMatch: ['**/test/**/*.spec.ts'],
-    collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts'],
+    collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts', '!src/**/index.ts'],
     coverageDirectory: 'coverage',
     coverageReporters: ['text', 'lcov'],
     moduleNameMapper: {
         '^@mark/(.*)$': '<rootDir>/../$1/src',
     },
     rootDir: './',
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: 80,
+        },
+        './src/**/index.ts': {
+            branches: 0,
+            functions: 0,
+            lines: 0,
+            statements: 0,
+        },
+    },
 }; 
