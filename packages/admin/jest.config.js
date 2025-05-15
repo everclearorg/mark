07fa8b40135c6bf013dab3709bb6d13299856c58
Layne Haber <layne.haber@gmail.com>
2025-05-15 16:27:28 -0600

    feat: admin api unit tests

--- packages/admin/jest.config.js
+++ packages/admin/jest.config.js
@@ -5,6 +5,8 @@ module.exports = {
     collectCoverageFrom: [
         'src/**/*.ts',
         '!src/types.ts', // Usually, type definitions are not included in coverage
+        '!src/init.ts',
         '!src/index.ts',
+        '!src/**/index.ts',
     ],
 };
