ff7fb130c3cca1ea7095e495e1ee83af7288e46e
Layne Haber <layne.haber@gmail.com>
2025-06-12 18:35:47 -0600

    feat: add safe methods to txservice

--- packages/adapters/chainservice/src/index.ts
+++ packages/adapters/chainservice/src/index.ts
@@ -1,7 +1,14 @@
 import { providers, Signer, constants } from 'ethers';
-import { ChainService as ChimeraChainService } from '@chimera-monorepo/chainservice';
+import {
+  ChainService as ChimeraChainService,
+  ITransactionReceipt,
+  WriteTransaction,
+} from '@chimera-monorepo/chainservice';
 import { ILogger } from '@mark/logger';
-import { createLoggingContext, ChainConfiguration } from '@mark/core';
+import { createLoggingContext, ChainConfiguration, TransactionRequest, WalletConfig } from '@mark/core';
+import SafeApiKit from '@safe-global/api-kit';
+import Safe from '@safe-global/protocol-kit';
+import { MetaTransactionData, OperationType } from '@safe-global/types-kit';
 
 export interface ChainServiceConfig {
   chains: Record<string, ChainConfiguration>;
@@ -14,10 +21,12 @@ export class ChainService {
   private readonly txService: ChimeraChainService;
   private readonly logger: ILogger;
   private readonly config: ChainServiceConfig;
+  private readonly signer: Signer;
 
   constructor(config: ChainServiceConfig, signer: Signer, logger: ILogger) {
     this.config = config;
     this.logger = logger;
+    this.signer = signer;
 
     // Convert chain configuration format to nxtp-txservice format
     const nxtpChainConfig = Object.entries(config.chains).reduce(
@@ -45,10 +54,74 @@ export class ChainService {
     });
   }
 
-  async submitAndMonitor(
+  async proposeMultisigTransaction(
     chainId: string,
-    transaction: providers.TransactionRequest,
-  ): Promise<providers.TransactionReceipt> {
+    request: TransactionRequest,
+    walletConfig: WalletConfig,
+  ): Promise<string> {
+    if (!walletConfig.safeAddress) {
+      throw new Error(`No safe address found in wallet configuration.`);
+    }
+    // Initialize the Protocol Kit with Owner A
+    const protocolKitOwnerA = await Safe.init({
+      provider: this.config.chains[chainId].providers[0],
+      safeAddress: walletConfig.safeAddress,
+    });
+
+    // Create a Safe transaction
+    const safeTransactionData: MetaTransactionData = {
+      to: request.to!,
+      value: request.value || '0',
+      data: request.data || '0x',
+      operation: OperationType.Call,
+    };
+
+    const safeTransaction = await protocolKitOwnerA.createTransaction({
+      transactions: [safeTransactionData],
+    });
+
+    const safeTxHash = await protocolKitOwnerA.getTransactionHash(safeTransaction);
+    const signature = await this.signer.signMessage(safeTxHash);
+
+    // Initialize the API Kit
+    const apiKit = new SafeApiKit({
+      chainId: BigInt(chainId),
+      txServiceUrl: this.config.chains[chainId].safeTxService,
+    });
+
+    // Send the transaction to the Transaction Service with the signature from Owner A
+    await apiKit.proposeTransaction({
+      safeAddress: walletConfig.safeAddress,
+      safeTransactionData: safeTransaction.data,
+      safeTxHash,
+      senderAddress: await this.signer.getAddress(),
+      senderSignature: signature,
+    });
+
+    return safeTxHash;
+  }
+
+  async getSafeTransactionReceipt(chainId: string, safeHash: string): Promise<undefined | ITransactionReceipt> {
+    // Initialize the API Kit
+    const apiKit = new SafeApiKit({
+      chainId: BigInt(chainId),
+      txServiceUrl: this.config.chains[chainId].safeTxService,
+    });
+
+    const safeTransaction = await apiKit.getTransaction(safeHash);
+    if (!safeTransaction || !safeTransaction.isExecuted) {
+      return undefined;
+    }
+
+    if (!safeTransaction.transactionHash) {
+      throw new Error(`Safe transaction is executed without transaction hash!`);
+    }
+
+    const receipt = await this.txService.getTransactionReceipt(+chainId, safeTransaction.transactionHash!);
+    return receipt;
+  }
+
+  async submitAndMonitor(chainId: string, transaction: TransactionRequest): Promise<providers.TransactionReceipt> {
     const { requestContext } = createLoggingContext('submitAndMonitor');
     const context = { ...requestContext, origin: 'chainservice' };
 
@@ -56,12 +129,12 @@ export class ChainService {
       throw new Error(`Chain ${chainId} not supported`);
     }
 
-    const writeTransaction = {
+    const writeTransaction: WriteTransaction = {
       to: transaction.to!,
       data: transaction.data! as `0x${string}`,
       value: transaction.value ? transaction.value.toString() : '0',
       domain: parseInt(chainId),
-      from: transaction.from,
+      from: transaction.from ?? undefined,
     };
     try {
       // TODO: once mark supports solana, need a new way to track gas here / update the type of receipt.
