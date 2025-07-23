e14ca0f9493d57e4ae7e4349ab19eb57710defe6
0xHarbs <jamesharbridge@outlook.com>
2025-07-23 11:14:56 +0100

    fix: linting

--- packages/adapters/rebalance/src/adapters/near/constants.ts
+++ packages/adapters/rebalance/src/adapters/near/constants.ts
@@ -1,5 +1,5 @@
-export const INTENTS_CONTRACT_ID = "intents.near";
-export const EOA_ADDRESS = "0xBc8988C7a4b77c1d6df7546bd876Ea4D42DF0837";
+export const INTENTS_CONTRACT_ID = 'intents.near';
+export const EOA_ADDRESS = '0xBc8988C7a4b77c1d6df7546bd876Ea4D42DF0837';
 
 /**
  * Maps external symbols to Near internal symbols
