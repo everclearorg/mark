1517ccde00fb9bf64cf301b6b7d80eda5d3a0268
just-a-node <eye1717@gmail.com>
2025-07-28 16:34:53 -0600

    feat: setup matoshi

---
+++ ops/mainnet/prod4/variables.tf
@@ -0,0 +1,98 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-southeast-1"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "mainnet"
+}
+
+variable "stage" {
+  description = "Stage name"
+  type        = string
+  default     = "prod4"
+}
+
+variable "domain" {
+  description = "Domain name"
+  type        = string
+  default     = "everclear.ninja"
+}
+
+variable "cidr_block" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "image_uri" {
+  description = "Full image name for the poller container (from CI pipeline)"
+  type        = string
+}
+
+# Poller-specific variables
+variable "invoice_age" {
+  description = "Maximum age of invoices to process (in seconds)"
+  type        = string
+  default     = "600"
+}
+
+variable "everclear_api_url" {
+  description = "URL of the Everclear API"
+  type        = string
+  default     = "https://api.everclear.org"
+}
+
+variable "relayer_url" {
+  description = "Optional relayer URL"
+  type        = string
+  default     = ""
+}
+
+variable "relayer_api_key" {
+  description = "Optional relayer API key"
+  type        = string
+  default     = ""
+  sensitive   = true
+}
+
+variable "supported_settlement_domains" {
+  description = "Comma-separated list of supported settlement domains"
+  type        = string
+  default     = "1,42161,10,8453,56,130,137,43114,48900,59144,81457,167000,534352,34443,324,33139,2020,80094,100,5000,146,57073,1399811149"
+}
+
+variable "supported_asset_symbols" {
+  description = "Comma-separated list of supported asset symbols"
+  type        = string
+  default     = "WETH,USDC,USDT,WBTC"
+}
+
+variable "log_level" {
+  description = "Log level (debug, info, warn, error)"
+  type        = string
+  default     = "debug"
+}
+
+variable "chain_ids" {
+  description = "Comma-separated list of chain IDs"
+  type        = string
+  default     = "1,42161,10,8453,56,130,137,43114,48900,59144,81457,167000,534352,34443,324,33139,2020,80094,100,5000,146,57073,1399811149"
+}
+variable "zone_id" {
+  description = "Route 53 hosted zone ID for the everclear.ninja domain"
+  default     = "Z0605920184MNEP9DVKIX"
+}
+
+variable "cert_arn" {
+  description = "ACM certificate"
+  default = "arn:aws:acm:sa-east-1:679752396206:certificate/cdd94d82-1d6d-47ab-a9ef-daef93734916"
+}
+
+variable "admin_image_uri" {
+  description = "The ECR image URI for the admin API Lambda function."
+  type        = string
+}
