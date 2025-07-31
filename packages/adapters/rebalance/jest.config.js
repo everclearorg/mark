358e5bb80c96917298d3a5927f09ba4d1289f8b7
just-a-node <eye1717@gmail.com>
2025-07-31 12:12:37 -0600

    fix: jest errors

--- packages/adapters/rebalance/jest.config.js
+++ packages/adapters/rebalance/jest.config.js
@@ -1,37 +1,41 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testMatch: ['**/test/**/*.spec.ts', '**/test/**/*.integration.spec.ts'],
-    testTimeout: 30000,
-    collectCoverageFrom: [
-        'src/**/*.ts',
-        '!src/**/*.d.ts',
-        '!src/**/index.ts',
-        '!src/**/types.ts',
-        '!src/adapters/across/utils.ts' // taken from across sdk
-    ],
-    coverageProvider: 'babel',
-    coverageDirectory: 'coverage',
-    coverageReporters: ['text', 'lcov'],
-    modulePathIgnorePatterns: ['<rootDir>/dist/'],
-    moduleNameMapper: {
-        '^@mark/core$': '<rootDir>/../../core/src',
-        '^@mark/core/(.*)$': '<rootDir>/../../core/src/$1',
-        '^@mark/(.*)$': '<rootDir>/../$1/src',
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/test/**/*.spec.ts', '**/test/**/*.integration.spec.ts'],
+  testTimeout: 30000,
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/**/index.ts',
+    '!src/**/types.ts',
+    '!src/adapters/across/utils.ts',
+    '!src/adapters/cctp/**/*.ts',
+  ],
+  coverageProvider: 'babel',
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+  modulePathIgnorePatterns: ['<rootDir>/dist/'],
+  moduleNameMapper: {
+    '^@mark/core$': '<rootDir>/../../core/src',
+    '^@mark/core/(.*)$': '<rootDir>/../../core/src/$1',
+    '^@mark/(.*)$': '<rootDir>/../$1/src',
+  },
+  // Make Jest resolve .ts before .js
+  moduleFileExtensions: [
+    'ts',
+    'tsx', // ← first in the list
+    'js',
+    'jsx',
+    'json',
+    'node',
+  ],
+  rootDir: './',
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 85,
+      lines: 85,
+      statements: 85,
     },
-    // Make Jest resolve .ts before .js
-    moduleFileExtensions: [
-        'ts', 'tsx',   // ← first in the list
-        'js', 'jsx',
-        'json', 'node'
-    ],
-    rootDir: './',
-    coverageThreshold: {
-        global: {
-            branches: 70,
-            functions: 85,
-            lines: 85,
-            statements: 85
-        }
-    }
+  },
 };
