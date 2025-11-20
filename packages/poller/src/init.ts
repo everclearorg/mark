c39c9681ed2b623dca9ac9b31f3eec41945b4b5b
bz <centauri@skiff.com>
2025-11-20 10:34:46 -0500

    fix: improve db migration path derive

--- packages/poller/src/init.ts
+++ packages/poller/src/init.ts
@@ -21,6 +21,7 @@ import { execSync } from 'child_process';
 import { bytesToHex, WalletClient } from 'viem';
 import { rebalanceMantleEth } from './rebalance/mantleEth';
 import { randomBytes } from 'crypto';
+import { resolve } from 'path';
 
 export interface MarkAdapters {
   purchaseCache: PurchaseCache;
@@ -104,15 +105,31 @@ async function runMigration(logger: Logger): Promise<void> {
       return;
     }
 
+
     // default to aws lambda environment path
     const db_migration_path = process.env.DATABASE_MIGRATION_PATH ?? '/var/task/db/migrations';
 
+    let cwdOption: { cwd?: string } = {};
+
+    // if an explicit db migration path is provided, set the cwd on execSync so it can be used for migrations
+    if (process.env.DATABASE_MIGRATION_PATH) {
+      const workspaceRoot = resolve(process.cwd(), '../..');
+      const databasePackageDir = resolve(workspaceRoot, 'packages/adapters/database');
+      cwdOption.cwd = databasePackageDir;
+    }
+
     logger.info(`Running database migrations from ${db_migration_path}...`);
 
-    const result = execSync(`dbmate --url "${databaseUrl}" --migrations-dir ${db_migration_path} --no-dump-schema up`, {
-      encoding: 'utf-8',
-    });
-    logger.info('Database migration completed', { output: result });
+    const result = execSync(
+      `dbmate --url "${databaseUrl}" --migrations-dir ${db_migration_path} --no-dump-schema up`,
+      {
+        encoding: 'utf-8',
+        ...cwdOption,
+      }
+    );
+
+      logger.info('Database migration completed', { output: result });
+
   } catch (error) {
     logger.error('Failed to run database migration', { error });
     throw new Error('Database migration failed - cannot continue with out-of-sync schema');
